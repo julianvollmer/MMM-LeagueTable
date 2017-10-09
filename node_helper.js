811e19bcbf851d959ec614fb5aa4145bc0465312
Julian Vollmer <>
2017-10-09 12:07:47 +0200

    implement callback

--- node_helper.js
+++ node_helper.js
@@ -9,7 +9,7 @@ module.exports = NodeHelper.create({
 		
 		if(notification === "CONNECTED"){
 			console.log(this.name + " ************* received a socket notification: " + notification + " - Payload: " + JSON.stringify(payload, null, 4 ));
-			fball = new Football(payload)
+			fball = new Football(payload, this);
 		}
 
 		if (notification === "LOG"){
@@ -25,21 +25,22 @@ module.exports = NodeHelper.create({
 
     start: function() {        
         this.sendSocketNotification("CONNECTED", "connected");
-        this.update();
     },
 
     update: function () {
     	var self = this;
         self.sendSocketNotification("UPDATEUI", "options");
-        setTimeout(function() {
-        	var table = fball.leagueTable.getTable();
-			for (var i = table.length - 1; i >= 0; i--) {
-				var item = table[i];
-				item.teamName = fball.team.getShortNameFromLongName(item.teamName);
-			}
-        	table.sort(fball.helper.sortBy('position', false, parseInt));
-        	self.sendSocketNotification("GET_LEAGUETABLE", table);
-        }, 20000);
+    	var table = fball.leagueTable.getTable();
+		for (var i = table.length - 1; i >= 0; i--) {
+			var item = table[i];
+			item.teamName = fball.team.getShortNameFromLongName(item.teamName);
+		}
+    	table.sort(fball.helper.sortBy('position', false, parseInt));
+    	self.sendSocketNotification("GET_LEAGUETABLE", table);
+    },
+
+    callbackFromFootballApi: function (argument) {
+    	this.update();
     }
 });
 
